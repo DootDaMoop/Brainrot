d72fae571657fe0d0996773f1f5d25626b030388
DootDaMoop <jasonkhotsombath@gmail.com>
2024-01-28 04:48:33 -0500

    Finished Page Turning System

--- Assets/Scripts/QuestionEntry.cs
+++ Assets/Scripts/QuestionEntry.cs
@@ -8,10 +8,16 @@ public class QuestionEntry : MonoBehaviour
 {
     public TMP_Text QuestionText;
     public ToggleGroup toggleGroup;
+    public int CorrectAnswerIndex;
 
     public void SetQuestiontext(string QuestionText) {
         this.QuestionText.text = QuestionText;
     }
+
+    public void SetCorrectAnswerIndex(int index) {
+        this.CorrectAnswerIndex = index;
+    }
+
     
     public ToggleGroup GetToggleGroup() {
         return this.toggleGroup;
--- Assets/Scripts/QuestionManager.cs
+++ Assets/Scripts/QuestionManager.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using TMPro;
+using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -13,6 +14,13 @@ public class QuestionManager : MonoBehaviour {
     public Toggle AnswerTogglePrefab;
     private string file = "studyguides.json";
 
+    [Header("Page Settings")]
+    public int QuestionsPerPage = 3;
+    public int CurrentPageIndex = 1;
+    public int TotalPages;
+    public Button NextPageButton;
+    public Button PrevPageButton;
+
     private void Start() {
         LoadQuestions();
     }
@@ -22,25 +30,68 @@ public class QuestionManager : MonoBehaviour {
         if(File.Exists(path)) {
             string json = File.ReadAllText(path);
             QuestionData questionData = JsonUtility.FromJson<QuestionData>(json);
+            TotalPages = Mathf.CeilToInt(questionData.questions.Count / QuestionsPerPage);
 
-            int i = 1;
-            // Creates Question and sets Question Text
-            foreach(Question question in questionData.questions) {
-                GameObject NewQuestion = Instantiate(QuestionPrefab, QuestionGrid.transform.position, Quaternion.identity, QuestionGrid.transform);
-                QuestionEntry questionEntry = NewQuestion.GetComponent<QuestionEntry>();
-                questionEntry.SetQuestiontext($"{i}. {question.QuestionText}");
-                AnswerToggleGroup = questionEntry.GetToggleGroup();
-                
-                // Creates Answer Choices for Question
-                float pos = 30;
-                foreach(string answer in question.Answers) {
-                    Toggle AnswerToggle = Instantiate(AnswerTogglePrefab, AnswerToggleGroup.transform);
-                    AnswerToggle.GetComponentInChildren<Text>().text = answer;
-                    AnswerToggle.GetComponent<RectTransform>().anchoredPosition = new Vector3(-170,pos,0);
-                    pos -= 35;
-                }
-                i++;
+            // Reset Questions
+            foreach(Transform question in QuestionGrid.transform) {
+                Destroy(question.gameObject);
             }
+
+            if(CurrentPageIndex == 0) {
+                PrevPageButton.gameObject.SetActive(false);
+            }
+            else if(CurrentPageIndex == TotalPages) {
+                NextPageButton.gameObject.SetActive(false);
+            } else {
+                NextPageButton.gameObject.SetActive(true);
+                PrevPageButton.gameObject.SetActive(true);
+            }
+
+            LoadPage(CurrentPageIndex,questionData);
+        }
+    }
+
+    public void LoadPage(int page, QuestionData questionData) {
+        
+        // Index Variables
+        int StartIndex = page * QuestionsPerPage;
+        int EndIndex = Mathf.Min(StartIndex + QuestionsPerPage, questionData.questions.Count);
+
+        for(int i = StartIndex; i < EndIndex; i++) {
+            Debug.Log($"Index: {i}");
+            Question question = questionData.questions[i];
+            GameObject NewQuestion = Instantiate(QuestionPrefab, QuestionGrid.transform.position, Quaternion.identity, QuestionGrid.transform);
+            QuestionEntry questionEntry = NewQuestion.GetComponent<QuestionEntry>();
+            questionEntry.SetQuestiontext($"{i+1}. {question.QuestionText}");
+            AnswerToggleGroup = questionEntry.GetToggleGroup();
+            
+            // Creates Answer Choices for Question
+            float pos = 30;
+            foreach(string answer in question.Answers) {
+                Toggle AnswerToggle = Instantiate(AnswerTogglePrefab, AnswerToggleGroup.transform);
+                AnswerToggle.GetComponentInChildren<Text>().text = answer;
+                AnswerToggle.group = AnswerToggleGroup;
+                AnswerToggle.isOn = false;
+                AnswerToggle.GetComponent<RectTransform>().anchoredPosition = new Vector3(-170,pos,0);
+                pos -= 35;
+            }
+            questionEntry.SetCorrectAnswerIndex(question.CorrectAnswer);
+            
+        }
+    }
+
+    // Page Functions
+    public void NextPage() {
+        if(CurrentPageIndex < TotalPages) {
+            CurrentPageIndex++;
+            LoadQuestions();
+        }
+    }
+
+    public void PreviousPage() {
+        if(CurrentPageIndex > 0) {
+            CurrentPageIndex--;
+            LoadQuestions();
         }
     }
 }
